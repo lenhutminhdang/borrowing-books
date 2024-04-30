0949412aa11a366238f4c48c9ae310bda300c162
lenhutminhdang <ginoace2k1@gmail.com>
2024-04-30 22:27:52 +0700

    style: change nav link color

--- frontend-reader/src/components/UI/Link.vue
+++ frontend-reader/src/components/UI/Link.vue
@@ -7,7 +7,7 @@ const props = defineProps(["routeName", "isButton", "classes"]);
     :to="{ name: routeName }"
     active-class="text-main"
     :class="classes"
-    class="inline-block py-2 px-3 text-center rounded-md text-gray-700 hover:text-main outline-none outline-offset-0 focus:text-yellow-400"
+    class="inline-block py-2 px-3 text-center rounded-md text-gray-700 hover:text-main outline-none outline-offset-0"
   >
     <slot />
   </router-link>
